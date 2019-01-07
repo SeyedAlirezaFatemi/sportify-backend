b42792d390eaa88360167609f2b746b691fd2c13
Alireza <alirezafa@rocketmail.com>
2019-01-07 20:34:20 +0330

    rename.

--- sportify/wsgi.py
+++ sportify/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sportify_backend project.
+WSGI config for sportify project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportify_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportify.settings')
 
 application = get_wsgi_application()
