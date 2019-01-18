90c07d10ee1a837ba683654de0a945a3e06a759f
Alireza <alirezafa@rocketmail.com>
2019-01-18 22:19:21 +0330

    :art: Improving structure / format of the code.

--- data/News.py
+++ data/News.py
@@ -1,3 +1,18 @@
 from news.models import Tag
 
-tag1 = Tag(title='iran')
+tag1 = Tag(title='Iran')
+tag1.save()
+tag2 = Tag(title='Spain')
+tag2.save()
+tag3 = Tag(title='National')
+tag3.save()
+tag4 = Tag(title='Global')
+tag4.save()
+tag5 = Tag(title='2019')
+tag5.save()
+tag6 = Tag(title='2018')
+tag6.save()
+tag7 = Tag(title='Rain')
+tag7.save()
+
+
--- news/models.py
+++ news/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Tag(models.Model):
     title = models.CharField(max_length=20, unique=True)
 
+    def __str__(self):
+        return self.title
+
 
 class News(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
@@ -15,9 +18,22 @@ class News(models.Model):
     tags = models.ManyToManyField(to=Tag)
     related_news = models.ManyToManyField(to="self")
 
+    class Meta:
+        ordering = ["pub_date"]
+        verbose_name_plural = "News"
+
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     text = models.CharField(max_length=500)
     pub_date = models.DateTimeField()
     news = models.ForeignKey(to=News, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["pub_date"]
+
+    def __str__(self):
+        return self.author
