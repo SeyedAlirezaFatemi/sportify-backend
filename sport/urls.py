be2e4ff0d5b21c62368437b2cbc375110f926bc6
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-01-21 23:27:00 +0330

    Related news to player

--- sport/urls.py
+++ sport/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('soccer/statistic/<int:pk>', views.SoccerPlayerStatistics.as_view(), name='soccer_player_statistic'),
     path('basketball/statistic/<int:pk>', views.BasketballPlayerStatistics.as_view(), name='basketball_player_statistic'),
+    path('related_news/<int:pk>', views.PlayerRelatedNews.as_view(), name='related_news')
 ]
--- sport/views.py
+++ sport/views.py
@@ -1,7 +1,10 @@
+from django.db.models import Q
 from rest_framework import generics
 
 from sport.models import *
+from news.models import News
 from sport.serializers.player_serializer import *
+from news.serializers import NewsSerializer
 
 
 class BasketballPlayerStatistics(generics.RetrieveAPIView):
@@ -12,3 +15,15 @@ class BasketballPlayerStatistics(generics.RetrieveAPIView):
 class SoccerPlayerStatistics(generics.RetrieveAPIView):
     queryset = SoccerPlayerSeason.objects.all()
     serializer_class = SoccerPlayerSeasonSerializer
+
+
+class PlayerRelatedNews(generics.ListAPIView):
+    serializer_class = NewsSerializer
+
+    def get_queryset(self):
+        player_id = self.kwargs['pk']
+        person = Person.objects.get(id=player_id)
+        name = person.name
+        queryset = News.objects.filter(Q(title__contains=name) | Q(brief__contains=name)
+                                       | Q(text__contains=name) | Q(tags__title__contains=name))
+        return queryset
