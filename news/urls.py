9ffc6ec8f1c2331a3b27a507ef1e3819fb67765f
Alireza <alirezafa@rocketmail.com>
2019-01-21 19:56:04 +0330

    News detail.

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('latest/', views.LatestNews.as_view(), name='latest_news')
+    path('latest/', views.LatestNews.as_view(), name='latest_news'),
+    path('detail/<int:pk>/', views.NewsDetail.as_view(), name='news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -4,6 +4,11 @@ from news.models import News
 from news.serializers import NewsSerializer
 
 
+class NewsDetail(generics.RetrieveAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+
 class LatestNews(generics.ListAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
