5e86f89138af33d4b95a649f53e52545cd6054c0
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-01-21 23:56:12 +0330

    Minor improvements

--- sport/urls.py
+++ sport/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('soccer/statistic/<int:pk>/', views.SoccerPlayerStatistics.as_view(), name='soccer_player_statistic'),
     path('basketball/statistic/<int:pk>/', views.BasketballPlayerStatistics.as_view(),
          name='basketball_player_statistic'),
-    path('soccer/statistic/<int:pk>', views.SoccerPlayerStatistics.as_view(), name='soccer_player_statistic'),
-    path('basketball/statistic/<int:pk>', views.BasketballPlayerStatistics.as_view(), name='basketball_player_statistic'),
-    path('related_news/<int:pk>', views.PlayerRelatedNews.as_view(), name='related_news'),
+    path('soccer/statistic/<int:pk>/', views.SoccerPlayerStatistics.as_view(), name='soccer_player_statistic'),
+    path('basketball/statistic/<int:pk>/', views.BasketballPlayerStatistics.as_view(), name='basketball_player_statistic'),
+    path('related_news/<int:pk>/', views.PlayerRelatedNews.as_view(), name='related_news'),
     path('soccer/player/<int:pk>/', views.SoccerPlayerInfo.as_view(), name='soccer_player_info'),
-    path('basketball/player/<int:pk>/', views.BasketballPlayerInfo.as_view(), name='basketball_player_info')
+    path('basketball/player/<int:pk>/', views.BasketballPlayerInfo.as_view(), name='basketball_player_info'),
+    path('person/images/<int:pk>/')
 ]
