9d3fd1bc464b73ecf25a79cc5da31038bb872c1a
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-01-21 21:09:13 +0330

    Soccer player statistics

--- sport/urls.py
+++ sport/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('statistic/', views.PlayerStatistics.as_view(), name='player_statistic'),
+    path('soccer/statistic/', views.SoccerPlayerStatistics.as_view(), name='soccer_player_statistic'),
+    path('basketball/statistic/', views.BasketballPlayerStatistics.as_view(), name='basketball_player_statistic'),
 ]
--- sport/views.py
+++ sport/views.py
@@ -7,10 +7,21 @@ from .serializers.player_serializer import *
 
 class BasketballPlayerStatistics(generics.RetrieveAPIView):
     model = BasketballPlayer
-    serializer_class = BasketballPlayerSeasonSerializer
+    serializer_class = BasketballPlayerSerializer
 
     def get_queryset(self):
         queryset = BasketballPlayer.objects.all()
+        player_id = self.request.query_params.get('player_id')
+
+        return queryset.get(id=player_id)
+
+
+class SoccerPlayerStatistics(generics.RetrieveAPIView):
+    model = SoccerPlayer
+    serializer_class = SoccerPlayerSerializer
+
+    def get_queryset(self):
+        queryset = SoccerPlayer.objects.all()
         player_id = self.request.query_params.get('player_id')
 
         return queryset.get(id=player_id)
