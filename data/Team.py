1880465663012a19fab460cdabf64d52020480ba
Alireza <alirezafa@rocketmail.com>
2019-01-22 17:06:41 +0330

    Improved data.

--- data/Team.py
+++ data/Team.py
@@ -1,6 +1,8 @@
+from django.utils import timezone
+
 from authentication.models import User
 from news.models import News
-from sport.models import Team, SoccerTeam
+from sport.models import SoccerTeam
 
 team1 = SoccerTeam(name='Bayern')
 team1.save()
@@ -8,6 +10,7 @@ team1.save()
 user1 = User.objects.get(pk=1)
 user1.save()
 
-news_3 = News(author=user1, title='I am happy in la', brief='Bayern hsjdhasudh ashdhan '
-              , text='Lebron James uadkjas ndjn asjndkjasn dkj', pub_date=timezone.now())
+news_3 = News(author=user1, title='I am happy in la', brief='This is Stupid! '
+              , text='Lebron James went home. Nobody really cares about him. He died peacefully.',
+              pub_date=timezone.now())
 news_3.save()
