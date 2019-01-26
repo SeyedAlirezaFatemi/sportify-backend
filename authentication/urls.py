55bae587f2b1ceb0f4d0872ca7c9d4614a77e3e5
Alireza <alirezafa@rocketmail.com>
2019-01-26 19:10:58 +0330

    improved authentication.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from rest_framework.authtoken import views
+
+from authentication.views import CustomAuthToken
 
 urlpatterns = [
-    url(r'^api-token-auth/', views.obtain_auth_token)
+    url(r'^api-token-auth/', CustomAuthToken.as_view())
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,25 @@
 from rest_auth.registration.views import RegisterView
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
 
 from authentication.models import User
 
 
 class CustomRegisterView(RegisterView):
     queryset = User.objects.all()
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
