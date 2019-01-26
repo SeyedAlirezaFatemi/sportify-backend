e10be922e0690c5ef6f5d7b239ef74fd934e1f23
Alireza <alirezafa@rocketmail.com>
2019-01-26 20:17:32 +0330

    urls updated.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.urls import path
 
 from authentication.views import CustomAuthToken
 
 urlpatterns = [
-    url(r'^api-token-auth/', CustomAuthToken.as_view())
+    url('api-token-auth/', CustomAuthToken.as_view()),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
