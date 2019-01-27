0475d55d9c52910ae08a94adb01e8b3e35b4f141
Alireza <alirezafa@rocketmail.com>
2019-01-27 17:20:42 +0330

    url for forgot password.

--- sportify/urls.py
+++ sportify/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('authentication.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('news/', include('news.urls')),
     path('sport/', include('sport.urls')),
 ]
