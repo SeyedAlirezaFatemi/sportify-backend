0a250cd2d4889bdb5150aa3d1c161b77b43a034b
Alireza <alirezafa@rocketmail.com>
2019-01-31 13:11:21 +0330

    Comments are shown.

--- news/serializers.py
+++ news/serializers.py
@@ -4,22 +4,23 @@ from authentication.serializers import CustomUserDetailsSerializer
 from news.models import Comment, News, Tag
 
 
-class NewsSerializer(ModelSerializer):
-    author = CustomUserDetailsSerializer()
-
-    class Meta:
-        model = News
-        fields = (
-            'id', 'sport', 'author', 'title', 'brief', 'text', 'pub_date', 'image', 'tags', 'related_news', 'comments')
-        depth = 1
-
-
 class CommentSerializer(ModelSerializer):
     author = CustomUserDetailsSerializer()
 
     class Meta:
         model = Comment
         fields = '__all__'
+
+
+class NewsSerializer(ModelSerializer):
+    author = CustomUserDetailsSerializer()
+    comments = CommentSerializer(many=True)
+
+    class Meta:
+        model = News
+        fields = (
+            'id', 'sport', 'author', 'title', 'brief', 'text', 'pub_date', 'image', 'tags', 'related_news', 'comments')
+        depth = 2
 
 
 class TagSerializer(ModelSerializer):
